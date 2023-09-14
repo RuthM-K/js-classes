461a1bb6a48e982b84f1a6db233c13930b07470b
Ruth <ruthmusyoka2000@gmail.com>
2023-09-14 05:28:21 +0300

    intro to js

---
+++ intro-to-js.js
@@ -0,0 +1,39 @@
+console.log(3 * (10 - 4))
+
+// Constant expression
+3
+"Hello"
+
+// Assignement expression
+result = 3 * (10-4)
+// to get the output of the above expression;
+// so the result, is a variable name. a variable name is assigned a valau
+result
+console.log(result)
+
+// looup expression; a avariable has been assigned a value
+result
+
+// Mutability and immutability
+/**
+ * Mutability - the value can change
+ * Immutability- the value cannot change
+ */
+
+// let keyword- mutable; the age will change the next year
+let age = 20
+age
+
+age = 21 
+console.log (age) 
+
+// const keyword - //Immutable. Name will not change with time
+const name = "Ruth" 
+console.log (name) 
+
+// var keyword- never use
+var fullname = "Musyoka"
+fullname
+
+var fullname = "Isika"
+console.log (fullname)
