620a98ebd789a89fe86465890cab0f7e5476fb3a
Ruth <ruthmusyoka2000@gmail.com>
2023-09-14 07:25:29 +0300

    strings.js

---
+++ strings.js
@@ -0,0 +1,14 @@
+// strings
+//string interpolation - injecting of values into a string
+// only works with back ticks
+
+const firstName = 'John'
+const lastName = 'Doe'
+const fullName = `John ${lastName} of age ${100-76}` 
+
+console.log(fullName)
+
+//string concatenation - joining strings together using (+)
+const newFullName = firstName + ' ' + lastName;
+
+console.log(newFullName)
